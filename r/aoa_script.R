2d452090efd44c2ba8f30c180c15015b9af31467
AlexanderPilz <51150328+xcomagent95@users.noreply.github.com>
2021-11-10 17:02:23 +0100

    Update aoa_script.R

--- r/aoa_script.R
+++ r/aoa_script.R
@@ -4,11 +4,14 @@ library(caret) #caret-Package for performing training
 library(sp) #sp-Package for handlig spatial datasets
 library(rgdal) #rgdal-Packge for performing spatial operations
 library(sf) #sf-package ???
-library(sp)
 library(rstac) #rstac for accessing STAC-Catalogue 
+
+#install devtools and gdalUtils
 #install.packages("devtools")
 #devtools:::install_github("gearslaboratory/gdalUtils")
 library(gdalUtils) #gdalUtil-Package for tranforming images
+
+#install gdalcubes
 #install.packages("gdalcubes")
 library(gdalcubes) #
 
