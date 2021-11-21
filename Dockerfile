837d713655948277fa0c0d776a826e0bd6aec58f
Noel Schnierer <noel.schnierer@gmail.com>
2021-11-21 19:41:01 +0100

    enhance docker setup

--- Dockerfile
+++ Dockerfile
@@ -3,11 +3,11 @@ FROM rocker/r-base:4.1.2
 WORKDIR /app
 
 # See: https://github.com/nodesource/distributions/blob/master/README.md
-curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+RUN curl -fsSL https://deb.nodesource.com/setup_lts.x | bash -
 
-# Install deps for R packages
 RUN apt-get update && apt-get install -y \
-    nodejs
+    nodejs \
+    npm
 
 COPY package*.json ./
 # Install with a clean slate
--- src/app.js
+++ src/app.js
@@ -2,11 +2,17 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
+require("dotenv").config();
+
 const app = express();
 
 // Set file size limit to 5mb
 app.use(bodyParser.json({ limit: "5mb", type: "application/json" }));
 
 app.use(cors());
+
+app.get("/info", function (req, res) {
+  res.send(`Environment: ${process.env.NODE_ENV || "-"}`);
+});
 
 module.exports = app;
