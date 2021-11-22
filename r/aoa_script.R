8e380a2e9007559c923dd924db574967a556cfaf
AlexanderPilz <51150328+xcomagent95@users.noreply.github.com>
2021-11-22 13:09:16 +0100

    get Images for AOI and sample Polygons

--- r/aoa_script.R
+++ r/aoa_script.R
@@ -31,6 +31,7 @@ library(mapview)
 
 #aoi and polygons
 samplePolygons <- read_sf('samplePolygons.geojson') #sample Polygons
+samplePolygon_bbox <- st_bbox(samplePolygons, crs = 4326)
 aoi <- read_sf('aoi.geojson', crs = 4326) #AOI
 aoi_bbox <- st_bbox(aoi, crs = 4326) #BBox of AOI
 resolution <- 50 #Resolutin of the Output-Image
@@ -38,35 +39,34 @@ cloud_cover <- 15 #Threshold for Cloud-Cover in Sentinel-Images
 t0 <- "2020-01-01"
 t1 <- "2020-12-31"
 timeframe <- paste(t0, '/', t1, sep ="")
+assets = c("B01","B02","B03","B04","B05","B06", "B07","B08","B8A","B09","B11","SCL")
+stac = stac("https://earth-search.aws.element84.com/v0")
 
 #mapview(st_geometry(samplePolygons)) 
 #mapview(st_geometry(aoi))
 #mapview(st_bbox(aoi))
 
-#gdalcubes
-stac = stac("https://earth-search.aws.element84.com/v0")
-
-items <- stac %>%
+####################Get Image-Data for AOI
+items_aoi <- stac %>%
   stac_search(collections = "sentinel-s2-l2a-cogs",
               bbox = c(aoi_bbox[1],aoi_bbox[2],aoi_bbox[3],aoi_bbox[4]), #Anfrage mit AOI-BBox
               datetime = timeframe,
               limit = 100) %>%
   post_request() 
-items
+items_aoi
 
-assets = c("B01","B02","B03","B04","B05","B06", "B07","B08","B8A","B09","B11","SCL")
-collection = stac_image_collection(items$features, asset_names = assets, 
+collection_aoi = stac_image_collection(items_aoi$features, asset_names = assets, 
                             property_filter = function(x) {x[["eo:cloud_cover"]] < cloud_cover})
-collection
+collection_aoi
 
 #Transformation of the BBOX
-targetSystem <- toString(items$features[[1]]$properties$`proj:epsg`) #read EPSG-Code of Sentinel-Images
+targetSystem <- toString(items_aoi$features[[1]]$properties$`proj:epsg`) #read EPSG-Code of Sentinel-Images
 targetString <- paste('EPSG:', targetSystem) #transform EPSG-Code to String
 aoi_transformed <- st_transform(aoi, as.numeric(targetSystem)) #transform AOI to Sentinel-Image EPSG
 aoi_bbox_tranformed <- st_bbox(aoi_transformed, crs = as.numeric(targetSystem)) #derive BBox of transformed AOI
 
 
-cube_view = cube_view(srs = targetString,  extent = list(t0 = t0, t1 = t1,
+cube_view_aoi = cube_view(srs = targetString,  extent = list(t0 = t0, t1 = t1,
                                                  left = aoi_bbox_tranformed[1], 
                                                  right = aoi_bbox_tranformed[3],  
                                                  top = aoi_bbox_tranformed[4], 
@@ -77,19 +77,58 @@ cube_view = cube_view(srs = targetString,  extent = list(t0 = t0, t1 = t1,
                                                  aggregation = "median", 
                                                  resampling = "average")
 
-S2.mask = image_mask("SCL", values=c(3,8,9)) # clouds and cloud shadows
+S2.mask = image_mask("SCL", values=c(3,8,9)) #clouds and cloud shadows
 
 gdalcubes_options(threads = 8) #set Threads for raster cube 
 
-cube_raster = raster_cube(collection, cube_view, mask = S2.mask) %>%
+cube_raster_aoi = raster_cube(collection_aoi, cube_view_aoi, mask = S2.mask) %>%
   select_bands(c("B02","B03","B04")) %>%
   reduce_time(c("median(B02)", "median(B03)", "median(B04)")) 
   
 (cube_raster %>%
   plot(rgb = 3:1, zlim=c(0,1800)) ) %>% system.time()
 
+####################Get Image-Data for sample Polygons
+items_poly <- stac %>%
+  stac_search(collections = "sentinel-s2-l2a-cogs",
+              bbox = c(samplePolygon_bbox[1], samplePolygon_bbox[2], samplePolygon_bbox[3], samplePolygon_bbox[4]), #Anfrage mit AOI-BBox
+              datetime = timeframe,
+              limit = 100) %>%
+  post_request() 
+items_poly
 
-############################################################################################################
+collection_poly = stac_image_collection(items_poly$features, asset_names = assets, 
+                                       property_filter = function(x) {x[["eo:cloud_cover"]] < cloud_cover})
+collection_poly
+
+#Transformation of the BBOX
+targetSystem <- toString(items_poly$features[[1]]$properties$`proj:epsg`) #read EPSG-Code of Sentinel-Images
+targetString <- paste('EPSG:', targetSystem) #transform EPSG-Code to String
+samplePolygons_transformed <- st_transform(samplePolygons, as.numeric(targetSystem)) #transform AOI to Sentinel-Image EPSG
+samplePolygons_bbox_tranformed <- st_bbox(samplePolygons_transformed, crs = as.numeric(targetSystem)) #derive BBox of transformed AOI
+
+
+cube_view_poly = cube_view(srs = targetString,  extent = list(t0 = t0, t1 = t1,
+                                                             left = samplePolygons_bbox_tranformed[1], 
+                                                             right = samplePolygons_bbox_tranformed[3],  
+                                                             top = samplePolygons_bbox_tranformed[4], 
+                                                             bottom = samplePolygons_bbox_tranformed[2]),
+                          dx = resolution, 
+                          dy = resolution, 
+                          dt = "P1D", 
+                          aggregation = "median", 
+                          resampling = "average")
+
+S2.mask = image_mask("SCL", values=c(3,8,9)) #clouds and cloud shadows
+
+gdalcubes_options(threads = 8) #set Threads for raster cube 
+
+cube_raster_poly = raster_cube(collection_poly, cube_view_poly, mask = S2.mask) %>%
+  select_bands(c("B02","B03","B04")) %>%
+  reduce_time(c("median(B02)", "median(B03)", "median(B04)")) %>%
+  plot(rgb = 3:1, zlim=c(0,1800)) %>% system.time()
+
+##########################################non working part##################################################################
 #///////
 sentinal2aImage <- #Sentinel2A Image for the Region the Polygons are in
 trainigData <- extract(sentinal2aImage, samplePolygons, df=TRUE) #Extract Training Samples
