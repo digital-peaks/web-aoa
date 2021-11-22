2e672217ed2e6ddd246f56d324d5375aa67a12a7
AlexanderPilz <51150328+xcomagent95@users.noreply.github.com>
2021-11-22 13:15:28 +0100

    Update aoa_script.R

--- r/aoa_script.R
+++ r/aoa_script.R
@@ -1,35 +1,28 @@
 #Packages
 setwd("~/GitHub/web-aoa/r")
 # setwd("~/Desktop/WS21:22/Geosoft_2/Github/web-aoa/r") # Josi 
-
 library(CAST) #CAST-Package for performing AOA
 library(caret) #caret-Package for performing training
 library(sp) #sp-Package for handlig spatial datasets
 library(rgdal) #rgdal-Packge for performing spatial operations
 library(sf) #sf-package for performing spatial operation on spheroids
 library(rstac) #rstac for accessing STAC-Catalogue 
-
 #install devtools and gdalUtils
 #install.packages("devtools")
 #devtools:::install_github("gearslaboratory/gdalUtils")
 library(gdalUtils) #gdalUtil-Package for tranforming images
-
 #install gdalcubes 
 #install.packages("gdalcubes")
 #install.packages(c("sf", "stars", "magick", "rmarkdown", "ncdf4", "Rcpp", "jsonlite", "RcppProgress", "rstac", "tmap"))
 library(gdalcubes) #for creating data cubes (Mean over many Images to reduce cloud related noise)
-
 #install.packages("rjson")
 library(rjson)
-
 #install.packages("ggplot2")
 library(ggplot2)
-
-# Auch ganz nice
 #install.packages("mapview")
 library(mapview)
 
-#aoi and polygons
+#Parameters
 samplePolygons <- read_sf('samplePolygons.geojson') #sample Polygons
 samplePolygon_bbox <- st_bbox(samplePolygons, crs = 4326)
 aoi <- read_sf('aoi.geojson', crs = 4326) #AOI
@@ -44,7 +37,8 @@ stac = stac("https://earth-search.aws.element84.com/v0")
 
 #mapview(st_geometry(samplePolygons)) 
 #mapview(st_geometry(aoi))
-#mapview(st_bbox(aoi))
+mapview(st_bbox(aoi))
+mapview(st_bbox(samplePolygons))
 
 ####################Get Image-Data for AOI
 items_aoi <- stac %>%
@@ -83,15 +77,13 @@ gdalcubes_options(threads = 8) #set Threads for raster cube
 
 cube_raster_aoi = raster_cube(collection_aoi, cube_view_aoi, mask = S2.mask) %>%
   select_bands(c("B02","B03","B04")) %>%
-  reduce_time(c("median(B02)", "median(B03)", "median(B04)")) 
-  
-(cube_raster %>%
-  plot(rgb = 3:1, zlim=c(0,1800)) ) %>% system.time()
+  reduce_time(c("median(B02)", "median(B03)", "median(B04)")) %>%
+  plot(rgb = 3:1, zlim=c(0,1800)) %>% system.time()
 
 ####################Get Image-Data for sample Polygons
 items_poly <- stac %>%
   stac_search(collections = "sentinel-s2-l2a-cogs",
-              bbox = c(samplePolygon_bbox[1], samplePolygon_bbox[2], samplePolygon_bbox[3], samplePolygon_bbox[4]), #Anfrage mit AOI-BBox
+              bbox = c(samplePolygon_bbox[1], samplePolygon_bbox[2], samplePolygon_bbox[3], samplePolygon_bbox[4]),
               datetime = timeframe,
               limit = 100) %>%
   post_request() 
@@ -113,11 +105,11 @@ cube_view_poly = cube_view(srs = targetString,  extent = list(t0 = t0, t1 = t1,
                                                              right = samplePolygons_bbox_tranformed[3],  
                                                              top = samplePolygons_bbox_tranformed[4], 
                                                              bottom = samplePolygons_bbox_tranformed[2]),
-                          dx = resolution, 
-                          dy = resolution, 
-                          dt = "P1D", 
-                          aggregation = "median", 
-                          resampling = "average")
+                                                             dx = resolution, 
+                                                             dy = resolution, 
+                                                             dt = "P1D", 
+                                                             aggregation = "median", 
+                                                             resampling = "average")
 
 S2.mask = image_mask("SCL", values=c(3,8,9)) #clouds and cloud shadows
 
@@ -129,51 +121,6 @@ cube_raster_poly = raster_cube(collection_poly, cube_view_poly, mask = S2.mask) 
   plot(rgb = 3:1, zlim=c(0,1800)) %>% system.time()
 
 ##########################################non working part##################################################################
-#///////
-sentinal2aImage <- #Sentinel2A Image for the Region the Polygons are in
-trainigData <- extract(sentinal2aImage, samplePolygons, df=TRUE) #Extract Training Samples
-#////////
-
-aoibbox <- st_bbox(aoi)
-s <- stac("https://earth-search.aws.element84.com/v0") #link to STAC
-
-q <- s %>% # same like below but withou "post_request()"
-    stac_search(collections = "sentinel-s2-l2a-cogs",
-                bbox = c(aoibbox["xmin"],aoibbox["ymin"],
-                         aoibbox["xmax"],aoibbox["ymax"]),
-                datetime = "2021-06-01/2021-06-30",
-                limit = 500) 
-
-q$params$query = "{\"eo:cloud_cover\": {\"lt\": 10}}" # limitates the cloud coverage 
-
-items <- q %>% post_request()
-
-col <- stac_image_collection(items$features, property_filter = function(x) {x[["eo:cloud_cover"]] < 20}) # limitates the cloud coverage 
-S2.mask = image_mask("SCL", values=c(3,8,9)) # clouds and cloud shadows (?)
-
-v = cube_view(srs = "EPSG:3857",  
-              extent = list(t0 = "2021-06-01", t1 = "2021-06-30", 
-                            left = aoibbox["xmin"]-1000, right = aoibbox["xmax"]+1000,  
-                            top = aoibbox["ymax"]+1000, bottom = aoibbox["ymin"]-1000), 
-                            dx = 20, dy = 20, dt = "P1D", aggregation = "mean", resampling = "near") #dt = temporal extend
-
-c = raster_cube(col, v) %>%
-            select_bands(c("B02","B03","B04")) %>%
-            reduce_time(c("median(B02)", "median(B03)", "median(B04)")) %>%
-            plot(rgb = 3:1, zlim=c(0,1500)) %>% system.time() # Funktioniert noch nicht 
-
-#Retrieve Images in BBox
-# items = s %>% #(may need to bee transformed?)
-  # stac_search(collections = "sentinel-s2-l2a-cogs",
-    #           bbox = c(aoibbox["xmin"],aoibbox["ymin"],
-    #                    aoibbox["xmax"],aoibbox["ymax"]), 
-    #           datetime = "2021-06-01/2021-06-30",
-    #           limit = 500) %>%
-  # post_request() 
-
-#Build Collection
-collection = stac_image_collection(items$features)
-
 model <- train(trainigData[,predictors], #extracted RGB-Values?
                trainigData$classes, #? Classes for LU/LC?
                method="rf", #Random Forrest
