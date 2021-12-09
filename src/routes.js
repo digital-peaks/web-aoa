df498a99738d0e25485ed91d56c61fdc680a7132
Noel Schnierer <noel.schnierer@gmail.com>
2021-12-10 00:10:54 +0100

    add routes to upload, get and delete files

--- src/routes.js
+++ src/routes.js
@@ -5,5 +5,6 @@ const router = express.Router();
 // Add another ./module/module.routes file here:
 
 router.use("/", require("./job/job.routes"));
+router.use("/", require("./file/file.routes"));
 
 module.exports = router;
