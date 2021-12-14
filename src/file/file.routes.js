7ebbb201da91b21bb252395bbd983d9553f495c1
Noel Schnierer <noel.schnierer@gmail.com>
2021-12-14 14:06:24 +0100

    add swagger docs (#51)

--- src/file/file.routes.js
+++ src/file/file.routes.js
@@ -7,15 +7,73 @@ const upload = multer();
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * tags:
+ *   name: Files
+ */
+
+/**
+ * @swagger
+ * /files:
+ *   post:
+ *     summary: Upload file.
+ *     tags: [Files]
+ *     consumes:
+ *       - multipart/form-data
+ *     requestBody:
+ *       content:
+ *         multipart/form-data:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               file:
+ *                 type: string
+ *                 format: binary
+ *     responses:
+ *       "201":
+ *         description: Created
+ *         content:
+ *           application/json:
+ *             schema:
+ *                $ref: '#/components/schemas/File'
+ *       "400":
+ *         $ref: '#/components/responses/BadRequestException'
+ *       "500":
+ *         $ref: '#/components/responses/InternalServerErrorException'
+ */
 router.post(
   "/files",
   upload.single("file"),
   asyncHandler(async (req, res) => {
     const result = await FileService.uploadFile(req.file);
-    res.json(result);
+    res.status(201).json(result);
   })
 );
 
+/**
+ * @swagger
+ * /files/{name}:
+ *   get:
+ *     summary: Get file by name.
+ *     tags: [Files]
+ *     parameters:
+ *       - in: path
+ *         name: name
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: File name
+ *     responses:
+ *       "200":
+ *         description: OK
+ *       "400":
+ *         $ref: '#/components/responses/BadRequestException'
+ *       "404":
+ *         $ref: '#/components/responses/NotFoundException'
+ *       "500":
+ *         $ref: '#/components/responses/InternalServerErrorException'
+ */
 router.get(
   "/files/:name",
   asyncHandler(async (req, res) => {
@@ -24,6 +82,26 @@ router.get(
   })
 );
 
+/**
+ * @swagger
+ * /files:
+ *   get:
+ *     summary: Get all files.
+ *     tags: [Files]
+ *     responses:
+ *       "200":
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: array
+ *                items:
+ *                  $ref: '#/components/schemas/File'
+ *       "400":
+ *         $ref: '#/components/responses/BadRequestException'
+ *       "500":
+ *         $ref: '#/components/responses/InternalServerErrorException'
+ */
 router.get(
   "/files",
   asyncHandler(async (req, res) => {
@@ -32,6 +110,27 @@ router.get(
   })
 );
 
+/**
+ * @swagger
+ * /files/{name}:
+ *   delete:
+ *     summary: Delete file by name.
+ *     tags: [Files]
+ *     parameters:
+ *       - in: path
+ *         name: name
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: File name
+ *     responses:
+ *       "200":
+ *         description: No content
+ *       "404":
+ *         $ref: '#/components/responses/NotFoundException'
+ *       "500":
+ *         $ref: '#/components/responses/InternalServerErrorException'
+ */
 router.delete(
   "/files/:name",
   asyncHandler(async (req, res) => {
--- src/job/job.routes.js
+++ src/job/job.routes.js
@@ -6,6 +6,70 @@ const router = express.Router();
 
 // Add express request handlers:
 
+/**
+ * @swagger
+ * tags:
+ *   name: Jobs
+ */
+
+/**
+ * @swagger
+ * /jobs:
+ *   post:
+ *     summary: Create a job
+ *     tags: [Jobs]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - name
+ *               - use_lookup
+ *               - resolution
+ *               - cloud_cover
+ *               - start_timestamp
+ *               - end_timestamp
+ *               - sampling_strategy
+ *             properties:
+ *              name:
+ *                type: string
+ *              use_lookup:
+ *                type: boolean
+ *              resolution:
+ *                type: number
+ *              cloud_cover:
+ *                type: number
+ *              start_timestamp:
+ *                type: date-time
+ *              end_timestamp:
+ *                type: date-time
+ *              sampling_strategy:
+ *                type: string
+ *              use_pretrained_model:
+ *                type: boolean
+ *             example:
+ *               name: "Job name"
+ *               use_lookup: false
+ *               resolution: 10
+ *               cloud_cover: 15
+ *               start_timestamp: "2020-01-01T00:00:00.000Z"
+ *               end_timestamp: "2020-06-01T00:00:00.000Z"
+ *               sampling_strategy: "regular"
+ *               use_pretrained_model: false
+ *     responses:
+ *       "201":
+ *         description: Created
+ *         content:
+ *           application/json:
+ *             schema:
+ *                $ref: '#/components/schemas/Job'
+ *       "400":
+ *         $ref: '#/components/responses/BadRequestException'
+ *       "500":
+ *         $ref: '#/components/responses/InternalServerErrorException'
+ */
 router.post(
   "/jobs",
   // The asyncHandler helps us to use await inside `(req, res) => { ... }`
@@ -15,10 +79,37 @@ router.post(
     // Please keep this logic simple and just call the service function.
     // Validations or MongoDB requests should run inside the service.
     const result = await JobService.createJob(req.body);
-    res.json(result);
+    res.status(201).json(result);
   })
 );
 
+/**
+ * @swagger
+ * /jobs/{id}:
+ *   get:
+ *     summary: Get job by id.
+ *     tags: [Jobs]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Job id
+ *     responses:
+ *       "200":
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *                $ref: '#/components/schemas/Job'
+ *       "400":
+ *         $ref: '#/components/responses/BadRequestException'
+ *       "404":
+ *         $ref: '#/components/responses/NotFoundException'
+ *       "500":
+ *         $ref: '#/components/responses/InternalServerErrorException'
+ */
 router.get(
   "/jobs/:jobId",
   asyncHandler(async (req, res) => {
@@ -27,6 +118,30 @@ router.get(
   })
 );
 
+/**
+ * @swagger
+ * /jobs:
+ *   put:
+ *     summary: Update job.
+ *     tags: [Jobs]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Job'
+ *     responses:
+ *       "200":
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *                $ref: '#/components/schemas/Job'
+ *       "400":
+ *         $ref: '#/components/responses/BadRequestException'
+ *       "500":
+ *         $ref: '#/components/responses/InternalServerErrorException'
+ */
 router.put(
   "/jobs",
   asyncHandler(async (req, res) => {
@@ -35,6 +150,26 @@ router.put(
   })
 );
 
+/**
+ * @swagger
+ * /jobs:
+ *   get:
+ *     summary: Get all jobs
+ *     tags: [Jobs]
+ *     responses:
+ *       "200":
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: array
+ *                items:
+ *                  $ref: '#/components/schemas/Job'
+ *       "400":
+ *         $ref: '#/components/responses/BadRequestException'
+ *       "500":
+ *         $ref: '#/components/responses/InternalServerErrorException'
+ */
 router.get(
   "/jobs",
   asyncHandler(async (req, res) => {
@@ -43,6 +178,27 @@ router.get(
   })
 );
 
+/**
+ * @swagger
+ * /jobs/{id}:
+ *   delete:
+ *     summary: Delete job by id.
+ *     tags: [Jobs]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Job id
+ *     responses:
+ *       "200":
+ *         description: No content
+ *       "404":
+ *         $ref: '#/components/responses/NotFoundException'
+ *       "500":
+ *         $ref: '#/components/responses/InternalServerErrorException'
+ */
 router.delete(
   "/jobs/:jobId",
   asyncHandler(async (req, res) => {
