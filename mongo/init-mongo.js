3ff6e8a9c5e0b4f128981a0abef7cc44f09274cb
Noel Schnierer <noel.schnierer@gmail.com>
2022-01-10 21:38:43 +0100

    add envs to mongo init script
    * retry mongo connection

--- mongo/init-mongo.js
+++ mongo/init-mongo.js
@@ -1,10 +1,10 @@
 db.createUser({
-    user: "webaoa",
-    pwd: "webaoapw",
-    roles: [
-      {
-        role: "readWrite",
-        db: "webaoa",
-      },
-    ],
-  });
+  user: _getEnv("MONGO_INITDB_API_USERNAME"),
+  pwd: _getEnv("MONGO_INITDB_API_PASSWORD"),
+  roles: [
+    {
+      role: "readWrite",
+      db: _getEnv("MONGO_INITDB_DATABASE"),
+    },
+  ],
+});
